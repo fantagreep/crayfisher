1c9d7b9b4030c9f75a99f08afbe9889f072bfe26
fantagreep <bump.lk_k.1215@icloud.com>
2021-01-09 09:37:29 +0000

    run rubocop

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :user_signed_in?
 
   def create
-     @comment = current_user.comments.build(comment_params)
+    @comment = current_user.comments.build(comment_params)
     if @comment.save
       redirect_back(fallback_location: root_path)
     else
@@ -18,7 +18,8 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:content, :user_id, :post_id)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
+  end
 end
