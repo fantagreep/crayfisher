3b2d96aeb02ba3c80194d27350f2abfbce20c75b
fantagreep <bump.lk_k.1215@icloud.com>
2021-02-01 19:15:17 +0000

    ログインしてなくても投稿を確認できるようにしました

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,19 +3,19 @@ class StaticPagesController < ApplicationController
     @spots = Spot.all
     gon.spots = @spots
     @user = current_user
+    if params[:q] && params[:q].reject { |key, value| value.blank? }.present?
+      @q = Post.all.includes([:comments, :spot, :user, picture_attachment: :blob, user: { image_attachment: :blob }, comments: :user]).
+        ransack(params[:q])
+      @posts = @q.result(distinct: true).paginate(page: params[:page])
+    else
+      @q = Post.none.ransack
+      @posts = Post.all.includes([:comments, :spot, :user, picture_attachment: :blob, user: { image_attachment: :blob }, comments: :user])
+    end
+    @feed_items = @posts.paginate(page: params[:page])
     if user_signed_in?
       @post = current_user.posts.build
       @post.build_spot
       @comment = Comment.new
-      if params[:q] && params[:q].reject { |key, value| value.blank? }.present?
-        @q = Post.all.includes([:comments, :spot, :user, picture_attachment: :blob, user: { image_attachment: :blob }, comments: :user]).
-          ransack(params[:q])
-        @posts = @q.result(distinct: true).paginate(page: params[:page])
-      else
-        @q = Post.none.ransack
-        @posts = Post.all.includes([:comments, :spot, :user, picture_attachment: :blob, user: { image_attachment: :blob }, comments: :user])
-      end
-      @feed_items = @posts.paginate(page: params[:page])
     end
   end
 
