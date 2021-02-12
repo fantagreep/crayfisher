f845543f8c4c7e1f90d2a6c088be144dcb5d499d
fantagreep <bump.lk_k.1215@icloud.com>
2021-02-12 02:15:48 +0000

    remove password digestファイルを削除しました

--- db/migrate/20201227143608_remove_passworddigest_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemovePassworddigestFromUsers < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :users, :password_digest
-  end
-
-  def down
-    add_column :users, :password_digest, :string
-  end
-end
